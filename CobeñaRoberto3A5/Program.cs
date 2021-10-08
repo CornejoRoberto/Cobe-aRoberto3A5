41111038b75ca597c4f28e225b8fa07a01628fca
CobeñaCornejoRoberto <roberto_samp@outlook.com>
2021-10-08 17:41:09 -0500

    Agregar archivos de proyecto.

---
+++ CobeñaRoberto3A5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace RobertoCobeña3A5
+{
+	    class Program
+    {
+       static void Main(string[] args)
+	        {
+            Console.Write("Ingrese su número entero:");
+            int num1 = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= num1; i++){
+                for (int j = 1; j <= num1-i; j++)
+                    Console.Write(" ");
+                for (int j = 1; j <= 2*i-1; j++)
+                    Console.Write("*");
+                Console.Write("\n");}
+        }
+    }
+}
